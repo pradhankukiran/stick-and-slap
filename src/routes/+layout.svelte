0c944471ea8930c2b5b5931520d8c764b859db45
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-04-19 04:22:07 +0530

    fix: reference favicon at static path so vercel crawler can find it

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import archivoBlackUrl from '@fontsource/archivo-black/files/archivo-black-latin-400-normal.woff2?url';
 	import spaceGroteskUrl from '@fontsource-variable/space-grotesk/files/space-grotesk-latin-wght-normal.woff2?url';
 	import caveatUrl from '@fontsource/caveat/files/caveat-latin-400-normal.woff2?url';
@@ -9,7 +8,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 	<link rel="preload" as="font" type="font/woff2" href={archivoBlackUrl} crossorigin="anonymous" />
 	<link rel="preload" as="font" type="font/woff2" href={spaceGroteskUrl} crossorigin="anonymous" />
